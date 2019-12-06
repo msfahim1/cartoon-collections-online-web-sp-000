2a9076d214568f854d6d8c6a51123dc5f0aef911
Michael Fahim <michael_fahim@yahoo.com>
2019-12-06 01:57:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(names)# code an argument here
 
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer)# code an argument here
   # Your code here
+  planeteer.map { |n| n.upcase }
 end
 
 def long_planeteer_calls# code an argument here
