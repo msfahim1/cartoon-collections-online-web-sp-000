2eaad24ff6a88aae1fadcb19ed5304b63f5734fe
Michael Fahim <michael_fahim@yahoo.com>
2019-12-06 01:53:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def roll_call_dwarves(names, index)# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
   names.each_with_index { |item, index+1| p "#{index} #{item}" }
 
