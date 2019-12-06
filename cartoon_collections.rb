fcf7dd75c47673aaef8464a9566f008a7b8868be
Michael Fahim <michael_fahim@yahoo.com>
2019-12-06 01:58:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ end
 
 def summon_captain_planet(planeteer)# code an argument here
   # Your code here
-  array.collect do |name|
-     name.capitalize + "!"
+  planeteer.collect do |name|
+    planeteer.capitalize + "!"
    end
 end
 
