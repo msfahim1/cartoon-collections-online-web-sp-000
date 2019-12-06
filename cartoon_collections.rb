e06bab6c21e121fdeab898fc9966ae0e902913bc
Michael Fahim <michael_fahim@yahoo.com>
2019-12-06 02:02:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,9 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.length<3? true : false
-end
+  array.any? do |word|
+  word.length > 4
+  end end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
